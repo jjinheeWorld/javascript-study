0c609f898262fb296dba3f0e5da257c0e0cbb644
jjinheeWorld <wlsgml450@naver.com>
2022-04-19 21:07:29 +0900

    기본문법_숫자

---
+++ 01기본문법/기본문법_03숫자.js
@@ -0,0 +1,10 @@
+/**
+ * 숫자
+ */
+
+console.log(1 === 1.0); // true
+console.log(Math.pow(2, 9999)); // Math.pow(2, 9999) == 2^9999
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+// Not a Number
+console.log(isNaN("1")); // false
