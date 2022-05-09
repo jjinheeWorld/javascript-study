91a85157e45b6e7b82e2953b1aa858c676108989
jjinheeWorld <wlsgml450@naver.com>
2022-05-09 16:54:46 +0900

    변수_변수선언및할당

---
+++ 03변수/변수_01변수선언및할당.js
@@ -0,0 +1,19 @@
+// 변수
+// 값을 저장하는 공간
+// 자료를 저장할 수 있는 이름이 주어진 기억장소
+
+// 변수 선언과 값의 할당
+let a = 0;
+console.log(a); // 0
+
+// 값의 재할당
+a = 1;
+console.log(a); // 1
+
+// 변수 선언
+let b;
+console.log(b); // undefined
+
+// 값의 할당
+b = 2;
+console.log(b); // 2
