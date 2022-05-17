ebdbc2bd3ada7f30733643da4f4972ddbd953b60
jjinheeWorld <wlsgml450@naver.com>
2022-05-17 23:48:49 +0900

    변수_객체타입

---
+++ 03변수/변수_07객체타입.js
@@ -0,0 +1,26 @@
+let name = 'apple';
+let color = 'red';
+let display = '🍎';
+let orangeName = 'orange';
+
+// 연관된 데이터를 함께 묶어서 보관할 수 있는 복합 데이터인 객체를 만들어보자.
+let apple = {
+  name: 'apple',
+  color: 'red',
+  display: '🍎',
+};
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+// 이번에는 오렌지 객체를 만들어보자.
+let orange = {
+  name: '오렌지',
+  color: 'orange',
+  display: '🍊',
+};
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
+console.log(orange.display);
